3bf94f2f96ad923ca7c8953ce0f33ceed467f70b
ivan <ivanbelousov14@gmail.com>
2022-05-15 18:23:09 +0300

    work is done! ver.2

--- app.py
+++ app.py
@@ -70,6 +70,7 @@ class MovieView(Resource):
     def get(self):
         director_id = request.args.get("director_id")
         genre_id = request.args.get("genre_id")
+        page = request.args.get("page")
         movies = Movie.query
         try:
             if director_id:
@@ -288,4 +289,4 @@ class DirectorView(Resource):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
